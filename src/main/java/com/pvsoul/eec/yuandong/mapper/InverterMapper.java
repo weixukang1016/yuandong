2d1636db927fe8a41fecc658228568b581aa817b
wei_health <78266110@qq.com>
2021-01-22 18:59:42 +0800

    提交代码

--- src/main/java/com/pvsoul/eec/yuandong/mapper/InverterMapper.java
+++ src/main/java/com/pvsoul/eec/yuandong/mapper/InverterMapper.java
@@ -2,7 +2,6 @@ package com.pvsoul.eec.yuandong.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pvsoul.eec.yuandong.entity.Inverter;
-import com.pvsoul.eec.yuandong.entity.Inverter;
 
 public interface InverterMapper extends BaseMapper<Inverter> {
     int deleteByPrimaryKey(Object id);
