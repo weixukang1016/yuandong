c27e36f15e9f5705dc8361106add544f48008aa6
wei_health <78266110@qq.com>
2021-02-17 17:21:44 +0800

    提交filter

---
+++ src/main/java/com/pvsoul/eec/yuandong/filter/CorsFilter.java
@@ -0,0 +1,50 @@
+package com.pvsoul.eec.yuandong.filter;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers","User-Agent,Origin,Cache-Control,Content-type,Date,Server,withCredentials,AccessToken");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials","true");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        //httpServletResponse.setHeader("Access-Control-Max-Age", "1209600");
+        //httpServletResponse.setHeader("Access-Control-Expose-Headers","accesstoken");
+        //httpServletResponse.setHeader("Access-Control-Request-Headers","accesstoken");
+        //httpServletResponse.setHeader("Expires", "-1");
+        //httpServletResponse.setHeader("Cache-Control", "no-cache");
+        //httpServletResponse.setHeader("pragma", "no-cache");
+
+        /*
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        List<String> authorizeList = new ArrayList<String>();
+        if (!"OPTIONS".equals(httpServletRequest.getMethod())) {// OPTIONS方法不要拦截，不然跨域设置不成功
+            if(!authorizeList.contains(request.getParameter("key"))){ //当请求的key不在我的授权列表 key值可以自己设定 用来识别是否为授权
+                return;
+            }
+        }
+        */
+        chain.doFilter(request, response);
+    }
+
+
+
+}
